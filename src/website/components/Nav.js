87a99da29eda497db0dbba25319e3245cf6a4ae8
shobkeyy <shobkyy@gmail.com>
2023-02-06 17:24:21 +0200

    'auth'

--- src/website/components/Nav.js
+++ src/website/components/Nav.js
@@ -43,7 +43,10 @@ const Nav = ({ active }) => {
 
             <div id='navMb' className='nav_links-group__inActive nav_links-pc'>
                 {
-                    currentUser?.uid === 'Ac0cCKWKxUWfaGe2HcHL1LX2TWD2' || currentUser?.uid === 'OqWEn6fJcZNgyLEskceO6RCa1qV2' || currentUser?.uid === '14ntzuPO1safPwDfmKDrTp2CcQ63' ?
+                    currentUser?.uid === 'Ac0cCKWKxUWfaGe2HcHL1LX2TWD2'
+                        || currentUser?.uid === 'OqWEn6fJcZNgyLEskceO6RCa1qV2'
+                        || currentUser?.uid === '14ntzuPO1safPwDfmKDrTp2CcQ63'
+                        || currentUser?.uid === 'u0kCx1kiHkavId1Boefqu4TFuhF3' ?
                         <div className='mb-nav_links-group'>
                             <Link to="/dashboard" className='nav_link'> Dashboard  <RiArrowDropRightLine className='nav_arr-ico' /> </Link>
                             <br className='disnone-pc' />
